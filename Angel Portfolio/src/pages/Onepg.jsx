59e5619fb4557af710fc880928678e1c474cb409
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2026-02-19 19:57:39 +0400

    updated

--- Angel Portfolio/src/pages/Onepg.jsx
+++ Angel Portfolio/src/pages/Onepg.jsx
@@ -3,14 +3,14 @@ import { Link, Navigate } from 'react-router-dom';
 import './Onepg.css';
 const Onepg = () => {
   return (
-    <main className='container'>
+    <main className="container">
         <img src="src/design/flower.png" alt="Flower" className="flower" />
  <div className="about-section">
   <img src="/profile.jpg" alt="Profile Picture" className="profile" />
     < div className="text-container">
-    <h1 className='heading'>About</h1>
+    <h1 className="heading">About</h1>
       
-      <p className='about'>
+      <p className="about">
         Angel Sara Mathew 
 Bsc Creative Computing graduate. Passionate about making videos and websites and designing, crafting, writing stories, and taking photos and videos.</p>
      
@@ -19,12 +19,22 @@ Bsc Creative Computing graduate. Passionate about making videos and websites and
     </div>
    
       <img src="src/design/petals.png" alt="Petals" className="petals" />
-       <div className='buttons'>
-      <button className='University_projects '>University Projects</button>
-      <button className='University_events'>University Events</button>
-      <button className='Personal_works'>Personal Works</button>
-      <button className='Certificates'>Certificates</button>
+       <div className="buttons">
+      <button className="sphere">University Projects</button>
+      <button className="sphere">University Events</button>
+      <button className="sphere">Personal Works</button>
+      <button className="sphere">Certificates</button>
     </div>
+  <div className="label">
+    <div className="line"></div>
+    <div className="label_content">
+      <div className="Sparkle"></div>
+        <h2 className='title1'> University projects</h2>
+      </div>
+     <div className="line"></div>
+    
+
+   </div>
   </main>
    
   );
