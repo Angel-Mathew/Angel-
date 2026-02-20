fb59d3d6b38e4148cfd3dc9ea7f7c9d1c2a8a9da
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2026-02-20 15:35:56 +0400

    updated

--- Angel Portfolio/src/pages/Onepg.jsx
+++ Angel Portfolio/src/pages/Onepg.jsx
@@ -4,7 +4,7 @@ import './Onepg.css';
 const Onepg = () => {
   return (
     <main className="container">
-        <img src="src/design/flower.png" alt="Flower" className="flower" />
+     
  <div className="about-section">
   <img src="/profile.jpg" alt="Profile Picture" className="profile" />
     < div className="text-container">
@@ -18,23 +18,53 @@ Bsc Creative Computing graduate. Passionate about making videos and websites and
       
     </div>
    
-      <img src="src/design/petals.png" alt="Petals" className="petals" />
+     
        <div className="buttons">
-      <button className="sphere">University Projects</button>
+      <button className="sphere" onClick={() => document.getElementById('projects-section').scrollIntoView({ behavior:'smooth'})}>University Projects</button>
       <button className="sphere">University Events</button>
       <button className="sphere">Personal Works</button>
       <button className="sphere">Certificates</button>
     </div>
-  <div className="label">
+
+  <div  className="label">
     <div className="line"></div>
     <div className="label_content">
       <div className="Sparkle"></div>
         <h2 className='title1'> University projects</h2>
       </div>
      <div className="line"></div>
-    
+  </div>
 
-   </div>
+  <section id= "projects-section" className="projects">
+    <div className='uniprojects'>
+      <div className="individualprojects">
+        <h3 className="title2">Individual Projects</h3>
+         <h2 className='subtitle'>TimeLapse video</h2>
+        <div className='card'>
+         
+         <video controls className="project-video"><source src= "public\assets\Universityprojects\time lapse sunset.mp4"/></video>
+        </div>
+          <h2 className='subtitle'>Bitsy game Finding Wifi</h2>
+        <div className='card'>
+        
+         <video controls className="project-video"><source src= "public\assets\Universityprojects\Bitsy game Finding Wifi.mp4"/></video>
+        </div>
+        
+        <h2 className='subtitle'>Application</h2>
+        <div className='card'>
+        <video controls className="project-video"><source src= "public\assets\Universityprojects\NaN calculator cocomix app.mp4"/></video>
+        </div>
+       <div className='card'>
+        
+         <video controls className="project-video"><source src= "public\assets\Universityprojects\walkthrough of multiple app (Diaries to Desire).mp4"/></video>
+        </div>
+
+      </div>
+       
+     
+
+    </div>
+  </section>
   </main>
    
   );
